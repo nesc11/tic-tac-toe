70e5000c81f1a1f24a9060ba732ecac9db97d36b
nescpro <salazarnestor1996@gmail.com>
2023-10-10 21:14:06 -0500

    Create the logic to store the state of the game on localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,21 @@ import { checkWinner } from './logic/board'
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
-  const [winner, setWinner] = useState(null)
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = window.localStorage.getItem('board')
+    if (boardFromLocalStorage) {
+      return JSON.parse(boardFromLocalStorage)
+    }
+    return Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? turnFromStorage : TURNS.X
+  })
+  const [winner, setWinner] = useState(() => {
+    const winnerFromStorage = window.localStorage.getItem('winner')
+    return winnerFromStorage ? winnerFromStorage : null
+  })
 
   const updateBoard = index => {
     if (board[index] || winner) return
@@ -19,10 +31,16 @@ function App() {
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-    setTurn(prevTurn => prevTurn === TURNS.X ? TURNS.O : TURNS.X)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
+      window.localStorage.setItem('winner', newWinner)
       confetti()
       setWinner(newWinner)
     } else if (!newBoard.includes(null)) {
@@ -34,6 +52,13 @@ function App() {
     setBoard(Array(9).fill(null))
     setWinner(null)
     setTurn(TURNS.X)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+    if (winner) {
+      window.localStorage.removeItem('winner')
+    }
+
   }
 
   return (
